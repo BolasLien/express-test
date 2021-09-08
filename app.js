0cf7084c5d109c338fc4158e7f3346619dc41b52
bolaslien <bolaslien@gmail.com>
2021-09-08 11:59:26 +0800

    serving a statice file(eg. css file)

--- app.js
+++ app.js
@@ -2,41 +2,21 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+// serving a static file
+// 指定 public 為靜態檔案位置，就可以載入 public 中的檔案 (例如 css file)
+// middleware
+app.use(express.static("public"))
+
 // Server side render, send file
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-// API, send object
-// app.get('/user', function (req, res) {
-//   const user = {
-//     name: 'Bolas',
-//     age: 18,
-//   }
-//   res.send(user)
-// })
-
 // routing in express
-// app.get('/bolas', function (req, res) {
-//   res.send("You are on the Bolas'es page.")
-// })
-
-// routing for all
-// app.get('*', function (req, res) {
-//   res.send("Cannot find what you want.")
-// })
-
-// routing for pattern
-// app.get('/product/:someProduct', (req, res) => {
-//   let { someProduct } = req.params
-//   res.send("This is " + someProduct)
-// })
-
-// routing for query
-app.get('/formHandling', (req, res) => {
-  let {fullname, age} = req.query
-  res.send("Your name is " + fullname + " and your age is " + age + " years old.")
+app.get('/bolas', function (req, res) {
+  res.send("You are on the Bolas'es page.")
 })
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000.')
